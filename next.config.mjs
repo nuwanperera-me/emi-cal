78438cf4f1366d2a45fee5776522c717316f668c
Nuwan Perera <nuwanperera613@gmail.com>
2024-08-03 23:38:21 +0530

    fix: warnings

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,18 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 
-
-import withPWA from "next-pwa"
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+};
 
 export default withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
-});
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
-
-// module.exports = withPWA(nextConfig);
-
-
-// import withPWA from "next-pwa";
-// // import runtimeCaching from "next-pwa/cache";
-
-// export default withPWA({
-// 	reactStrictMode: true,
-// 	pwa: {
-// 		dest: "public",
-// 		register: true,
-// 		skipWaiting: true,
-// 		buildExcludes: [/middleware-manifest.json$/]
-// 	}
-// });
+})(nextConfig);
