ba2e51b87c6332734901663800b2d64d1c45eda2
Nuwan Perera <nuwanperera613@gmail.com>
2024-08-08 23:44:18 +0530

    chore: enhance card

--- components/card.tsx
+++ components/card.tsx
@@ -12,22 +12,32 @@ export const AmountCard = ({
     currency: "USD",
   });
 
-
   const TotalPayment = emi * months;
   const Interest = TotalPayment - amount;
+
   const formatedPayemnt = USDollar.format(TotalPayment);
   const formatedEmi = USDollar.format(emi);
   const formatedAmount = USDollar.format(amount);
   const formatedInterest = USDollar.format(Interest);
 
-  const percentage = (Interest / TotalPayment * 100).toString().concat("%")
+  const percentage = ((Interest / TotalPayment) * 100).toString().concat("%");
 
+  const copyContent = async (text: string | number) => {
+    try {
+      await navigator.clipboard.writeText(text.toString());
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
 
   return (
-    <section className="flex w-full flex-col items-center gap-4- rounded-lg border border-border bg-secondary p-4">
-      <div className="text-center font-normal mb-12">
+    <section className="gap-4- flex w-full flex-col items-center rounded-lg border border-border bg-secondary p-4">
+      <div className="mb-12 text-center font-normal">
         <p className="mb-6 font-semibold">Equated Monthly Installment</p>
-        <p className="text-3xl font-semibold text-accent-foreground">
+        <p
+          className="cursor-pointer text-3xl font-semibold text-accent-foreground hover:underline"
+          onClick={() => copyContent(formatedEmi)}
+        >
           {formatedEmi}
         </p>
         <p>
@@ -36,9 +46,12 @@ export const AmountCard = ({
           {months % 12} month{months % 12 > 1 ? "s" : ""}
         </p>
       </div>
-      <div className="w-full mb-4">
+      <div className="mb-4 w-full">
         <div className="relative mb-2 h-2 w-full overflow-hidden rounded-sm bg-primary-foreground">
-          <div className="absolute right-0 h-full w-[] bg-accent" style={{width: percentage}}></div>
+          <div
+            className="absolute right-0 h-full bg-accent transition-transform duration-300"
+            style={{ width: percentage }}
+          ></div>
         </div>
         <div className="mb-1 flex justify-between text-xs">
           <p className="flex items-center justify-start gap-2">
@@ -54,19 +67,28 @@ export const AmountCard = ({
       <div className="flex w-full flex-col gap-1 text-left text-xs text-muted-foreground ">
         <p>
           Total Payment:{" "}
-          <span className=" font-semibold text-primary-foreground">
+          <span
+            className=" cursor-pointer font-semibold text-primary-foreground hover:underline"
+            onClick={() => copyContent(formatedPayemnt)}
+          >
             {formatedPayemnt}
           </span>
         </p>
         <p>
           Total Amount:{" "}
-          <span className=" font-semibold text-primary-foreground">
+          <span
+            className=" cursor-pointer font-semibold text-primary-foreground hover:underline"
+            onClick={() => copyContent(formatedAmount)}
+          >
             {formatedAmount}
           </span>
         </p>
         <p>
           Total Interest:{" "}
-          <span className="font-semibold text-primary-foreground">
+          <span
+            className="cursor-pointer font-semibold text-primary-foreground hover:underline"
+            onClick={() => copyContent(formatedInterest)}
+          >
             {formatedInterest}
           </span>
         </p>
