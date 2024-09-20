2ac2ead0b04a4bbc96c515cde81351a1c34d3a21
nuwan-perera <nuwanperera613@gmail.com>
2024-09-20 07:49:15 +0530

    feat: not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,10 @@
+import { TbError404 } from "react-icons/tb";
+
 export default function NotFoundPage() {
-  return (<main></main>)
+  return (
+    <main className="relative w-full flex flex-col items-center justify-center text-center mt-40">
+      <TbError404 className="w-32 h-32 md:w-96 md:h-96 text-zinc-700" />
+      <h1 className="absolute bottom-2 md:bottom-14 font-mono font-semibold text-4xl md:text-8xl text-nowrap z-10">Not found</h1>
+    </main>
+  );
 }
